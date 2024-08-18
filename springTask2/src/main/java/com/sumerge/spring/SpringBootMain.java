12719e0e917b56bd08f66ab0be0a31ca849127e1
Manar Khedr <77106419+Manar-Khedr@users.noreply.github.com>
2024-08-18 18:30:39 +0300

    Commit Task 69442 JPA

--- springTask2/src/main/java/com/sumerge/spring/SpringBootMain.java
+++ springTask2/src/main/java/com/sumerge/spring/SpringBootMain.java
@@ -2,8 +2,10 @@ package com.sumerge.spring;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.sumerge.spring3")
 public class SpringBootMain {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMain.class, args);
